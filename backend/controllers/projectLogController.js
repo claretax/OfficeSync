7828f57e292550aa8a6000a06a18cf3410d57264
claretax <avninaina223@gmail.com>
2025-05-14 16:47:57 +0530

    feat(logRoutes): add endpoint to fetch logs by project ID
    Introduce a new endpoint `/projectLogs/:projectId` to retrieve logs specific to a project. This enhances the API by allowing more granular log retrieval based on project ID, improving usability and flexibility for clients.

--- backend/controllers/projectLogController.js
+++ backend/controllers/projectLogController.js
@@ -13,15 +13,34 @@ const createProjectLog = async (req, res) => {
   }
 const getProjectLogs = async (req, res) => {
     try {
-      const { projectId } = req.params;
       const projectLogs = await ProjectLog.find();
+      if (!projectLogs) {
+        return res.status(404).json({ message: 'ProjectLogs not found' });
+      }
       res.status(200).json(projectLogs);
     }
     catch (err) {
       res.status(500).json({ message: err.message });
     }
   }
+//get project logs by projectId
+const getProjectLogsByProjectId = async (req, res) => {
+    try {
+      const { projectId } = req.params;
+      const projectLogs = await ProjectLog.find({ project: projectId });
+      if (!projectLogs) {
+        return res.status(404).json({ message: 'ProjectLogs not found' });
+      }
+      res.status(200).json(projectLogs);
+    }
+    catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  }
+
+
   module.exports = {
     createProjectLog,
-    getProjectLogs
+    getProjectLogs,
+    getProjectLogsByProjectId
   }
--- backend/routes/logRoutes.js
+++ backend/routes/logRoutes.js
@@ -1,7 +1,8 @@
 const routes = require('express').Router();
-const { getProjectLogs, createProjectLog } = require('../controllers/projectLogController');
+const { getProjectLogs, createProjectLog, getProjectLogsByProjectId } = require('../controllers/projectLogController');
 // project logs
-routes.get('/projectLogs/:projectId', getProjectLogs);
+routes.get('/projectLogs', getProjectLogs);
 routes.post('/projectLogs', createProjectLog);
+routes.get('/projectLogs/:projectId', getProjectLogsByProjectId);
 
 module.exports = routes;
